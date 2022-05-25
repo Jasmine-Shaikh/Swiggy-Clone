0166b2b1cf535d50f5b58da7148737eba5836711
Pawan kumar <pawanbtechcse752000@gmail.com>
2022-05-26 00:00:33 +0530

    something left

--- script/index.js
+++ script/index.js
@@ -1,101 +1,12 @@
-let restaurantsAvailable = [{
-    "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/obppgtgqevlsog4v7asi",
-    "title": "Basil Pesto Paneer Pizza (Thin Crust)",
-    "price": "292",
-    "desc": "Basil Pesto Paneer Pizza (Thin Crust)"
-  }, {
-    "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/euaev67hctjnqi0a5p26" ,
-    "title":"Caramelized Onion & Feta Cheese Pizza (Thin Crust)",
-    "price": "169",
-    "desc":"Caramelized Onion & Feta Cheese Pizza (Thin Crust)"
-  }, {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/l0udrkr0xchcdka2eaei",
-    "title": "Match-day Veg Pizza Fest",
-    "price": 327,
-    "desc": "Feast on thin crust Mother earth pizza and premium classic margherita pizza during an eventful match",
-  }, {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/obppgtgqevlsog4v7asi",
-    "title": "Caramelized Onion & Feta Cheese Pizza (Thin Crust)",
-    "price": 215,
-    "desc": " Caramelized Onion & Feta Cheese Pizza (Thin Crust)",
-   }, {
-     "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/xs2yrrmbov3ix9bmhnnq",
-     "title":"Italian Pink Sauce Penne Pasta with Paprika",
-     "price": 200,
-    "desc":"Pasta tossed in rich & cheesy sauce with Italian herbs & vegetables with a touch of Chef's special condiments",
-   }, {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/lw0w9wdv7poaipovtewi",
-    "title": "Elaichi Kesar Kulfi",
-    "price": 352,
-    "desc": "Rich and creamy kulfi, made with whole milk, healthy khandasari sugar, saffron and cardamom. Allergen information: Dairy."
-   },
-   {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/mvyebstvdcbsuzqwh8bp",
-    "title": "Creamy Tomato & BBQ Penne Pasta",
-    "price": 199,
-    "desc": "The tangy tomato, cheesy cream sauce and a hint of sweet BBQ sauce come together to make this heavenly pasta."
-   },
-   {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/oymfdcwwuxhka7dvimeu",
-    "title": "Ghar Ki Chicken Curry (with bone) with Paratha",
-    "price": 199,
-    "desc": "The tangy tomato, cheesy cream sauce and a hint of sweet BBQ sauce come together to make this heavenly pasta."
-   },
-   {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/d0nexdbrzukff4gwym2p",
-    "title": "Mangalorean Chicken (with bone) with Rice",
-    "price": 188,
-    "desc": "Our take on the mangalorean ghassi, chicken simmered in a fresh coconut and bedgi chili gravy, finished with garam masala. Served with rice. A hearty meal for 1."
-   },
-   {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/g44tyqms5ndfsvutlek0",
-    "title": "Mangalorean Chicken (with bone) with Rice",
-    "price": 188,
-    "desc": "Our take on the mangalorean ghassi, chicken simmered in a fresh coconut and bedgi chili gravy, finished with garam masala. Served with rice. A hearty meal for 1."
-   },
-   {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/vheazgqsiduyrqmair4t",
-    "title": "Baked Non-Veg Samosa Combo",
-    "price": 183,
-    "desc": "Enjoy samosa guilt free! Combo of bhuna chicken and chicken keema samosa anytime, any-day!"
-   },
-   {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/bj5nvhabjlbcmw6bq0kp",
-    "title": "Peri Peri Sauce Penne Pasta",
-    "price": 228,
-    "desc": "Peri-peri spice added to classic creamy sauce pasta makes it into an irresistible burst of flavor with every bite."
-   },
-   {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/l3xrxq8yyzjuiulr4t2h",
-    "title": "Peri Peri Sauce Penne Pasta",
-    "price": 328,
-    "desc": "Classy creamy spaghetti pasta cooked in rich alfredo sauce!"
-   },
-   {
-   "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/my4dsgwxfivgrknf580f",
-   "title": "Peri Peri Sauce Penne Pasta",
-   "price": 279,
-   "desc": "Slurp in the soft spaghetti cooked in spicy tomato basil sauce with perfect blend of flavours!"
-  },
-  {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/uxzv1pu8j1trzhvbrcsl",
-    "title": "Chocolate Shake",
-    "price": 92,
-    "desc": "Protein powered delicious chcocolat oatmeal shake on the go!"
-   },
-   {
-    "img": "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/vnpxficx4rteehadpfot",
-    "title": "Zero Sugar Cola Fizz",
-    "price": 50,
-    "desc": "Protein powered delicious chcocolat oatmeal shake on the go!"
-   },
+ async function fetchAndDisplayData(){
+   let url = "http://localhost:3000/restaurantsAvailable";
 
-   
- 
-
-  
-  ]
-
+   let res = await fetch(url);
+   console.log(res);
+   let data = await res.json();
+   console.log(data);
+   displayData(data);
+ }
 
   let show = document.getElementById("show");
   function displayData(data){
@@ -128,5 +39,6 @@ let restaurantsAvailable = [{
       show.append(box);
     });
   }
-  displayData(restaurantsAvailable);
+  // displayData(restaurantsAvailable);
+  fetchAndDisplayData();
   
